fc3df7386d2782b1e0ba56b4a5ebd57131196366
LawrenceQ <lawrenceqf@gmail.com>
2023-01-30 22:06:02 -0300

    Implementando interface de usuário em WPF

---
+++ DDDInPractice.Domain.Tests/SnackMachineSpecs.cs
@@ -0,0 +1,15 @@
+﻿using DDDInPractice.Logic;
+
+namespace DDDInPractice.Domain.Tests;
+
+public class SnackMachineSpecs
+{
+    [Fact]
+    public void Return_money_empties_money_in_tansaction()
+    {
+        var snackMachine = new SnackMachine();
+        snackMachine.InsertMoney(new Money(0, 0, 0, 1, 0, 0));
+
+
+    }
+}
