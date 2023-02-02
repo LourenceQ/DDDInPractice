52b0b8d0c39905f87b1c8b42d0fc2e7d562211bf
LawrenceQ <lawrenceqf@gmail.com>
2023-02-01 22:16:34 -0300

    implementando ORM NHibernate e fazendo configuração inicial

---
+++ DDDInPractice.Logic/SessionFactory.cs
@@ -0,0 +1,38 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Conventions.AcceptanceCriteria;
+using FluentNHibernate.Conventions.Helpers;
+using NHibernate;
+using System.Reflection;
+
+namespace DDDInPractice.Logic;
+
+internal static class SessionFactory
+{
+    public static ISessionFactory _factory;
+
+    internal static ISession OpenSession()
+    {
+        return _factory.OpenSession();
+    }
+
+    public static void Init(string connectionString)
+    {
+        _factory = BuilderSessionFactory(connectionString);
+    }
+
+    public static ISessionFactory BuilderSessionFactory(string connectionString)
+    {
+        FluentConfiguration configuration = Fluently.Configure()
+            .Database(MsSqlConfiguration.MsSql2012.ConnectionString(connectionString))
+            .Mappings(mbox => mbox.FluentMappings
+                .AddFromAssembly(Assembly.GetExecutingAssembly())
+                .Conventions.Add(ForeignKey.EndsWith("ID"),
+                ConventionBuilder.Property
+                    .When(criteria => criteria.Expect(x => x.Nullable, Is.Not.Set), x => x.Not.Nullable()))
+                .Conventions.Add<TableNameConvention>()
+                .Conventions.Add<HiLoConvention>());
+
+        return configuration.BuildSessionFactory();
+    }
+}
